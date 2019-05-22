c5c2b0c2d30e1e56b4b1f8f8d3be8889c6e750e9
Danylo <da12171998@gmail.com>
2019-05-22 16:05:45 +0200

    first step

--- searchingCurses/searchingCurces/searchingCurces/Program.cs
+++ searchingCurses/searchingCurces/searchingCurces/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,24 @@ namespace searchingCurses
     {
         static void Main(string[] args)
         {
+            Console.Write("artist = ");
+            string artist = Console.ReadLine();
+            Console.WriteLine("title = ");
+            string title = Console.ReadLine();
+            var songsLyrics = new SongLyrics(artist, title);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+
+  class SongLyrics
+    {
+        public SongLyrics(string artist, string title)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
         }
     }
 }
