54a2d8f926a1d3a537083778a314e5b57cf570d6
Danylo <da12171998@gmail.com>
2019-05-22 16:46:13 +0200

    add txt plik do debug

--- searchingCurses/searchingCurces/searchingCurces/Program.cs
+++ searchingCurses/searchingCurces/searchingCurces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,18 @@ namespace searchingCurses
             string artist = Console.ReadLine();
             Console.Write("title = ");
             string title = Console.ReadLine();
-            var songsLyrics = new SongLyrics(artist, title);
+            //var songsLyrics = new SongLyrics(artist, title);
+            var profanalityFinder = new ProfanalityFinder("no, kurwa");
             Console.WriteLine("Done");
             Console.ReadLine();
+        }
+    }
+
+    class ProfanalityFinder
+    {
+        public ProfanalityFinder(string text)
+        {
+            var dictFile = File.ReadAllText("profanitites.txt");
         }
     }
 }
